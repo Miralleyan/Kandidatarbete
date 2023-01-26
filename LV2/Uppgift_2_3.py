7f43a1f17c2365ce3e3d6b2eafe873ec078b2136
KarlEgeland <karleg@student.chalmers.se>
2023-01-26 16:07:00 +0100

    Task 2

--- LV2/Uppgift_2_3.py
+++ LV2/Uppgift_2_3.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
 # Check PyTorch version
@@ -9,22 +10,18 @@ print(torch.__version__)
 RANDOM_SEED = 42
 torch.manual_seed(RANDOM_SEED)
 
-class OurModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.p = nn.Parameter(torch.randn(1,
-                                                requires_grad=True,
-                                                dtype=torch.float))
-        self.q = nn.Parameter(torch.randn(1,
-                                                requires_grad=True,
-                                                dtype=torch.float))
-        self.u = nn.Parameter(torch.randn(1,
-                                                requires_grad=True,
-                                                dtype=torch.float))
-    
-    def  forward(self, x: torch.Tensor) -> torch.Tensor:
-        L = self.p*self.q + self.u*(self.p + self.q - 1)
-        external_grad = torch.tensor([1.])
-        L.backward(gradient = external_grad)
-        norm = torch.norm(torch.tensor(p.grad, q.grad, u.grad))
-        return norm
+p = torch.tensor(0.2, requires_grad=True)
+q = torch.tensor(0.2, requires_grad=True)
+u = torch.tensor(0.2, requires_grad=True)
+
+optimizer = torch.optim.Adam([p, q, u], lr=0.1)
+
+epochs = 100
+
+for epoch in range(epochs):
+    loss = (q + u) ** 2 + (p + u) ** 2 + (p + q - 1) ** 2
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    if epoch % 10 == 0:
+        print(f'Epoch: {epoch}\tLoss: {loss:.4f}\tP = {p.item():.4f}\tQ = {q.item():.4f}\tU = {u.item():.4f}')
