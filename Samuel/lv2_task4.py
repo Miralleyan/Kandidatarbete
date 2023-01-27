c5864fd1e4eec1722dcca44d2dbfc71d2d854fee
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-01-27 09:33:23 +0100

    Change to explicit gradient

--- Samuel/lv2_task4.py
+++ Samuel/lv2_task4.py
@@ -5,13 +5,10 @@ from matplotlib import pyplot as plt
 # Introduce slack variables, so that all constraints become equalities
 # Slack variables are basically non-negative variables that get added to the 
 # inequality constraints, so that they become equalities
-pars = torch.nn.Parameter(torch.tensor(0.5).repeat(11))
+pars = torch.nn.Parameter(torch.tensor([0.5, 0.5, 0., 0., 0., 0., 0., 0., 0.,\
+    0., 0.]))
 
-opt = torch.optim.Adam(params=[pars], lr=0.1)
-
-for epoch in range(100):
-    opt.zero_grad()
-    print(pars)
+def L(pars):
     # Clamp on slack variables
     with torch.no_grad():
         pars[7:].clamp_(0)
@@ -19,22 +16,35 @@ for epoch in range(100):
     L = pars[0]*pars[1] + pars[2]*(pars[0] + pars[1] - 1) +\
         pars[3]*(-pars[0] + pars[7]) + pars[4]*(pars[0] - 1 + pars[8]) +\
         pars[5]*(-pars[1] + pars[9]) + pars[6]*(pars[1] - 1 + pars[10])
+    return L
+
+def LNorm(pars : torch.tensor):
+    LVal = L(pars)
     # Take the gradient
-    L.backward()
-    parGrad = torch.nn.Parameter(pars.grad)
-    # Define the loss function as the squared norm of the gradient
-    loss = parGrad[0]**2 + parGrad[1]**2 + parGrad[2]**2 +\
+    parGrad = torch.gradient(LVal)
+    # Calculate squared norm of the gradient
+    Norm = parGrad[0]**2 + parGrad[1]**2 + parGrad[2]**2 +\
         parGrad[3]**2 + parGrad[4]**2 + parGrad[5]**2 +\
         parGrad[6]**2 + parGrad[7]**2 + parGrad[8]**2 +\
         parGrad[9]**2 + parGrad[10]**2
-    # Take the gradient of the loss function
+    return Norm
+
+opt = torch.optim.Adam(params=[pars], lr=0.1)
+
+for epoch in range(100):
+    opt.zero_grad()
+    loss = (pars[1]+pars[2]-pars[3]+pars[4])**2 + (pars[0]+pars[2]-pars[5]+pars[6])**2\
+        + (pars[0]+pars[1]-1)**2 + (-pars[0]+pars[7])**2\
+        + (pars[0]-1+pars[8])**2 + (-pars[1]+pars[9])**2\
+        + (pars[1]-1+pars[10])**2
     loss.backward()
     opt.step()
+    pq = [par.item() for par in pars[:2]]
+    plt.plot(pq[0], pq[1], ".")
 
 pars = pars.detach()
 pars[7:].clamp_(0)
 print(pars)
 
 plt.plot(np.linspace(0,1), np.ones(50)-np.linspace(0,1))
-plt.plot(pars[0], pars[1], ".")
 plt.show()
