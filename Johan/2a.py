60e8c475fd70d41997bb58a00a95ad48b7ba7d87
Miralleyan <odesjo@chalmers.se>
2023-02-05 23:19:31 +0100

    lr

--- Johan/2a.py
+++ Johan/2a.py
@@ -28,38 +28,3 @@ for st in range(9):#Bäst vid 9 men också bra med över 100
     mu=mu*2
     print(mu)'''
 
-
-
-import torch
-from torch import nn
-
-p0=torch.tensor(0.7,requires_grad=True)
-p=p0
-#optimizer=torch.optim.SGD([p],lr=0.001)
-lr=0.3
-optimizer=torch.optim.Adam([p],lr)
-steps=200
-
-def vart(p):
-    return p.item()*(1-p.item())+ 10**8*(p.item()<0 or p.item()>1)
-
-for step in range(steps):
-    optimizer.zero_grad() # otherwise, the grads on steps will sum up
-    var = p*(1-p)+10**8*(p<0 or p>1)
-    var.backward()
-
-    while True:
-        po=torch.tensor(p.item(),requires_grad=True) #save p
-        optimizer.step()
-        if vart(po)<vart(p):
-            for g in optimizer.param_groups:
-                g['lr'] = g["lr"]-0.0001
-            p=torch.tensor(po.item(),requires_grad=True)
-        else:
-            for g in optimizer.param_groups:
-                g['lr'] = lr
-            break
-    #Compare new p with old, if new is larger decrease steppsize
-    #0.7 is magic stepsize value
-    if step == step:
-        print(f'Step {step+1: 2}: p={p.item(): 0.4f} and variance is {var.item(): 1.4f}')
