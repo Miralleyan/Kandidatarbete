9161cc1892ea899627efaa67f946335289118b6d
Ryerzs <ryerzs@gmail.com>
2023-02-10 11:04:43 +0100

    Empty methods

--- Optimizer/pytorch_measure.py
+++ Optimizer/pytorch_measure.py
@@ -5,3 +5,27 @@ class pytorch_measure():
     weights = torch.nn.Parameter()
     def __init__(self):
         pass
+
+    def total_mass(self):
+        pass
+
+    def total_variation(self):
+        pass
+
+    def support(self):
+        pass
+
+    def positive_part(self):
+        pass
+
+    def negative_part(self):
+        pass
+
+    def put_mass(self):
+        pass
+
+    def take_mass(self):
+        pass
+
+    def sample(self):
+        pass
