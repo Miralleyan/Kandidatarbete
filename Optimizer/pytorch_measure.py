862fb851d2eaebf32ad6034e307e231b16f6389a
Ryerzs <ryerzs@gmail.com>
2023-02-10 16:04:52 +0100

    Create main function for class

--- Optimizer/pytorch_measure.py
+++ Optimizer/pytorch_measure.py
@@ -115,10 +115,14 @@ class Pytorch_measure:
         pass
 
 
-a=torch.tensor([0.1,0.1,0.3,0.1,0.4])
-b=torch.tensor([1.,2.,3.,4.,5.])
+def main():
+    a=torch.tensor([0.1,0.1,0.3,0.1,0.4])
+    b=torch.tensor([1.,2.,3.,4.,5.])
 
     c=Pytorch_measure(b,a)
 
-print(c.put_mass(0.2, 1))
-print(c)
+    print(c.put_mass(0.2, 1))
+    print(c)
+
+if __name__ == "__main__":
+    main()
