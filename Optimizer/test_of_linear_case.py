a4ded7e9bf45d849bb53ca6cff01680313326973
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-02-14 16:20:34 +0100

    Change so data is tensor

--- Optimizer/test_of_linear_case.py
+++ Optimizer/test_of_linear_case.py
@@ -1,4 +1,3 @@
-import sys
 import torch
 import pytorch_measure as pm
 import numpy as np
@@ -8,13 +7,13 @@ l = torch.tensor([0., 0.2, 0.4, 0.6, 0.8, 1.])
 w = torch.tensor([0.3, 0.15, 0.05, 0.05, 0.15, 0.3])
 measure = pm.PytorchMeasure(l, w)
 
-data = [0., 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 1.]
+data = torch.tensor([0., 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.6, 0.6, 0.8, 0.8, 1.])
 
 def loss_fn(w):
-    return -sum([torch.log(w[torch.nonzero(l==data[i]).item()]) for i in range(len(data))])
+    return -sum([torch.log(w[torch.nonzero(l==data[i].item()).item()]) for i in range(len(data))])
 
 print(loss_fn(measure.weights))
-measure.vizualize()
+measure.visualize()
 for epoch in range(1000):
     measure.step(loss_fn, 0.05)
-measure.vizualize()
+measure.visualize()
