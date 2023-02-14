05521937fc5adc6e25941e17ee4ad16be87b7982
Miralleyan <odesjo@chalmers.se>
2023-02-14 15:59:45 +0100

    s

--- Optimizer/linear_test.py
+++ Optimizer/linear_test.py
@@ -10,26 +10,28 @@ y=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(x-mu)**2/(2*sigma**2))#"Observed" data
 y/=sum(y) #Normalize
 
 
-w = torch.tensor([1/N]*N)#starting weights
-l = x#locations
-
-def loss_fn(w): #loss function
+def loss_fn(w):
     x_d=torch.nn.parameter.Parameter(x)
     y_d=torch.nn.parameter.Parameter(y)
     m=mu
     s=sigma
     #return -sum(-1/(2*s**2)*((y_d-torch.mul(x_d,w))**2))
     return sum((y-w)**2)/len(w)
-    return -sum([torch.log()])
 
 
-measure = pm.PytorchMeasure(l, w)#Our starting measure
-for epoch in range(2000):
-    measure.step(loss_fn, 0.001)
-    #print(measure)
-plt.scatter(x,y,zorder=2)
-print(measure.total_mass())
-d=measure.sample(1)
-print(d)
-measure.visualize()
 
+def test_step():
+    w = torch.tensor([1/N]*N)
+    l = x
+    measure = pm.PytorchMeasure(l, w)
+    for epoch in range(2000):
+        measure.step(loss_fn, 0.001)
+        #print(measure)
+    plt.scatter(x,y,zorder=2)
+    print(measure.total_mass())
+    d=measure.sample(1)
+    print(d)
+    measure.visualize()
+
+
+test_step()
