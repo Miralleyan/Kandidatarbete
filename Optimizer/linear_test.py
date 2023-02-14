5a39d166722d654d831c959587472a47468076fd
Miralleyan <odesjo@chalmers.se>
2023-02-14 16:21:55 +0100

    linear

--- Optimizer/linear_test.py
+++ Optimizer/linear_test.py
@@ -2,36 +2,34 @@ import torch
 import pytorch_measure as pm
 import numpy as np
 import matplotlib.pyplot as plt
+
+N=200
+x=torch.randn(N)
+
+
 mu=0
 sigma=1
-N=200
-x=torch.linspace(-10,10,N)
-y=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(x-mu)**2/(2*sigma**2))#"Observed" data
-y/=sum(y) #Normalize
+xt=torch.linspace(-10,10,N)
+yt=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(xt-mu)**2/(2*sigma**2))
+yt/=sum(yt) #Normalize
+
+
+w = torch.tensor([1/N]*N)#Weights
+l = xt#Locations
+measure = pm.PytorchMeasure(l, w)
 
 
 def loss_fn(w):
-    x_d=torch.nn.parameter.Parameter(x)
-    y_d=torch.nn.parameter.Parameter(y)
-    m=mu
-    s=sigma
-    #return -sum(-1/(2*s**2)*((y_d-torch.mul(x_d,w))**2))
-    return sum((y-w)**2)/len(w)
+    return sum((yt-w)**2)/len(w)
+    #return -sum([torch.log(w[torch.nonzero(l==x_d[i]).item()]) for i in range(len(x_d))])
 
 
 
-def test_step():
-    w = torch.tensor([1/N]*N)
-    l = x
-    measure = pm.PytorchMeasure(l, w)
-    for epoch in range(2000):
-        measure.step(loss_fn, 0.001)
-        #print(measure)
-    plt.scatter(x,y,zorder=2)
-    print(measure.total_mass())
-    d=measure.sample(1)
-    print(d)
-    measure.visualize()
+for epoch in range(2000):
+    measure.step(loss_fn, 0.001)
+    #print(measure)
+plt.scatter(xt,yt,zorder=2)
+print(1-measure.total_mass())
+measure.visualize()
 
 
-test_step()
