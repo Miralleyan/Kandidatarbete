9fb7d40063e0f7a0c74014031660e95a08e9a600
Miralleyan <odesjo@chalmers.se>
2023-02-14 15:25:58 +0100

    linear test works

--- Optimizer/linear_test.py
+++ Optimizer/linear_test.py
@@ -1,29 +1,35 @@
 import torch
 import pytorch_measure as pm
 import numpy as np
+import matplotlib.pyplot as plt
 mu=0
 sigma=1
-x=np.linspace(-3,3,100)
+N=200
+x=torch.linspace(-10,10,N)
 #print(x)
 
-y=1/(2*np.pi*sigma)*np.exp(-(x-mu)**2/(2*sigma**2))
+y=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(x-mu)**2/(2*sigma**2))
+y/=sum(y)
 #print(y)
 
 def loss_fn(w):
-    #x_d=torch.nn.parameter.Parameter(x)
-    #y_d=torch.nn.parameter.Parameter(y)
-    x_d=torch.tensor(x)
-    y_d=torch.tensor(y)
+    x_d=torch.nn.parameter.Parameter(x)
+    y_d=torch.nn.parameter.Parameter(y)
     m=mu
     s=sigma
-    return -sum(-1/(2*s)*np.exp(-(y_d-torch.prod(x_d,w))**2/(2*s**2)))
+    #return -sum(-1/(2*s**2)*((y_d-torch.mul(x_d,w))**2))
+    return sum((y-w)**2)
+
+
 
 def test_step():
-    w = torch.tensor([0.2, 0.2, 0.2, 0.2, 0.2])
-    l = torch.tensor([-2.,-1.,0.,1.,2.])
+    w = torch.tensor([1/N]*N)
+    l = x
     measure = pm.PytorchMeasure(l, w)
-    for epoch in range(100):
-        measure.step(loss_fn, 0.01)
-        print(measure)
+    for epoch in range(2000):
+        measure.step(loss_fn, 0.001)
+        #print(measure)
+    plt.scatter(x,y)
+    measure.visualize()
 
 test_step()
