88456248e5226f940c50c508cd8965ac5a3a29b8
Miralleyan <odesjo@chalmers.se>
2023-02-14 14:38:43 +0100

    s

--- Optimizer/linear_test.py
+++ Optimizer/linear_test.py
@@ -9,13 +9,19 @@ x=np.linspace(-3,3,100)
 y=1/(2*np.pi*sigma)*np.exp(-(x-mu)**2/(2*sigma**2))
 #print(y)
 
-def loss_fn():
-    return -sum(-1/(2))
+def loss_fn(w):
+    #x_d=torch.nn.parameter.Parameter(x)
+    #y_d=torch.nn.parameter.Parameter(y)
+    x_d=torch.tensor(x)
+    y_d=torch.tensor(y)
+    m=mu
+    s=sigma
+    return -sum(-1/(2*s)*np.exp(-(y_d-torch.prod(x_d,w))**2/(2*s**2)))
 
 def test_step():
     w = torch.tensor([0.2, 0.2, 0.2, 0.2, 0.2])
     l = torch.tensor([-2.,-1.,0.,1.,2.])
-    measure = pm.Pytorch_measure(l, w)
+    measure = pm.PytorchMeasure(l, w)
     for epoch in range(100):
         measure.step(loss_fn, 0.01)
         print(measure)
