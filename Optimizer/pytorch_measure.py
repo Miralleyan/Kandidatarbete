4a1196f5b1931d84928007dd4d1cd2c1044351ee
KarlEgeland <karleg@student.chalmers.se>
2023-02-16 14:55:26 +0100

    __str__ method update

--- Optimizer/pytorch_measure.py
+++ Optimizer/pytorch_measure.py
@@ -19,9 +19,13 @@ class PytorchMeasure:
         """
         Returns the locations and weights of the measure as a string.
         :returns: str
-        Responsibilty: Filip
+        Responsibilty: Filip, Karl
         """
-        return "Locations: " + self.locations.tolist().__str__() + "\nWeights: " + self.weights.tolist().__str__()
+
+        out = "\033[4mLocations:\033[0m     \033[4mWeights:\033[0m \n"
+        for i in range(len(self.weights)):
+            out += f'{self.locations[i].item(): < 10}     {self.weights[i].item(): < 10}\n'
+        return out
 
     def __repr__(self) -> str:
         """
@@ -195,7 +199,7 @@ def main():
     c = PytorchMeasure(b, a)
     #print(c.negative_part())
     #print(c.put_mass(0.9, 1))
-    #print(c)
+    print(c)
     test_sample()
 
 
