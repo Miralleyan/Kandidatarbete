ef2e8321d467d8a4b07f55cd954ccf368cb3318e
Miralleyan <odesjo@chalmers.se>
2023-02-16 16:04:04 +0100

    linear_test

--- Optimizer/linear_test.py
+++ Optimizer/linear_test.py
@@ -3,25 +3,21 @@ import pytorch_measure as pm
 import numpy as np
 import matplotlib.pyplot as plt
 
-N=50
-data=torch.randn(1000)
-
-
-mu=0
-sigma=1
-xt=torch.linspace(-10,10,N)
-yt=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(xt-mu)**2/(2*sigma**2))
-yt/=sum(yt) #Normalize
+N=21
+data=torch.randn(10000)
 
 
 w = torch.tensor([1/N]*N)#Weights
-l = xt#Locations
+l = torch.linspace(-4,4,N)
 w = torch.nn.parameter.Parameter(w)
 l = torch.nn.parameter.Parameter(l)
 
-measure = pm.Measure(l, w)
-opt=pm.Optimizer(measure)
 
+mu=0 #Create true values
+sigma=1
+x=torch.linspace(-4,4,N)
+y=1/(np.sqrt(2*np.pi)*sigma)*torch.exp(-(x-mu)**2/(2*sigma**2))
+y/=sum(y) #Normalize
 
 index = torch.argmin(abs(l-data.view(-1,1)), dim=1)
 def loss_fn(w):
@@ -30,14 +26,22 @@ def loss_fn(w):
     #return -sum([torch.log(w[torch.nonzero(l==data[i].item()).item()]) for i in range(len(data))])
 
 
-
-for epoch in range(2000):
+measure = pm.Measure(l, w)
+opt=pm.Optimizer(measure)
+for epoch in range(5000):
+    measure.zero_gradient()
     loss=loss_fn(measure.weights)
     loss.backward()
-    opt.step(lr=0.01)
+    opt.step(lr=0.0001)
     #print(measure)
-plt.scatter(xt,yt,zorder=2)
+plt.scatter(x,y,zorder=2)
 print(1-measure.total_mass())
 measure.visualize()
+
+
+plt.hist(measure.sample(10000),bins=20, density=True, range=[-4,4])
+plt.hist(torch.randn(10000),bins=20, density=True, range=[-4,4], alpha=0.5)
+plt.legend(['Model','True data'])
+plt.show()
 
 
