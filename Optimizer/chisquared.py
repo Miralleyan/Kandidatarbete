89e5c23b1bbbf2ef0ef131ee0ba749dc475fabce
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-03-09 15:24:37 +0100

    Change chi_squared

--- Optimizer/chisquared.py
+++ Optimizer/chisquared.py
@@ -1,17 +1,25 @@
 import scipy as sp
 import pytorch_measure as pm
 import torch
+from matplotlib import pyplot as plt
 
-l = torch.tensor([0., 0.2, 0.4, 0.6, 0.8, 1.])
+l = torch.tensor([-1., -0.6, -0.2, 0.2, 0.6, 1.])
 w = torch.tensor([0.3, 0.15, 0.05, 0.05, 0.15, 0.3])
 measure = pm.Measure(l, w)
 
-data = torch.randn(1000)
+data = torch.randn(100000)
+alpha = 0.01
+plt.hist(data)
+plt.show()
 
 def chi_squared(measure):
-    bins = torch.tensor([torch.argmin(abs(data[i] - measure[0].weights)) for i in range(len(data))])
-    bins_freq = []
-    print(measure[0.weights])
+    # Calculate frequencies of data
+    bins = torch.tensor([torch.argmin(abs(data[i] - measure[0].locations)) for i in range(len(data))])
+    bins_freq = bins.unique(return_counts=True)[1]/len(data)
+    # Label smoothing
+    bins_freq = bins_freq*(1-alpha)+alpha/len(bins_freq)
+    print(bins_freq)
+
     return sum(measure[0].weights**2/bins_freq)
 
 opt = pm.Optimizer(measure, lr=0.01)
