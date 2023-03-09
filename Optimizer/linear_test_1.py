13e9646fe44bbf489186e54038c66e8ab766fe3f
Miralleyan <odesjo@chalmers.se>
2023-03-09 15:24:29 +0100

    test

--- Optimizer/linear_test_1.py
+++ Optimizer/linear_test_1.py
@@ -20,19 +20,33 @@ x=torch.linspace(-4,4,N)
 y=1/(np.sqrt(2*np.pi))*torch.exp(-(x-mu)**2/(2*sigma**2))
 y/=sum(y) #Normalize
 
-h=0.2
+
 
 
 
 
 #index = torch.argmin(abs(l-data.view(-1,1)), dim=1)
 def loss_fn(w):
+    sam=w[0].sample(1000)
+    h=1.06*len(sam)**(-1/5)
+    
     def K(x):
-        return 1/(np.sqrt(2*np.pi))*torch.exp(-x**2/2)
+        return 1/(np.sqrt(2*np.pi*h))*torch.exp(-x**2/2)
 
     def KDE(x):
-        return torch.tensor([1/(N*h) *(K((xi-w[0].locations).sum()/h)).item() for xi in x], requires_grad=True)
-
+        
+        #sam=w[0].locations
+        #wei=w[0].weights
+        #print([(xi-sam) for xi in x])
+        
+        return torch.tensor([1/(len(sam)) *(K((xi-sam)/h)).sum().item() for xi in x], requires_grad=True)
+ 
+    #x=w[0].locations
+    #y=KDE(x).detach().numpy()
+    #x=x.detach().numpy()
+    #plt.scatter(x,y/sum(y))
+    #plt.show()
+    #print(-KDE(data).log().sum())
     return -KDE(data).log().sum()
     #return -w[0].weights[index].log().sum()
     #return sum((y-w)**2)/len(w)
@@ -51,7 +65,9 @@ def loss_fn(measures):
 
 lr=0.1
 measure = pm.Measure(l, w)
+#print(loss_fn([measure]))
 opt=pm.Optimizer([measure],lr=lr)
+
 
 '''
 for epoch in range(5000):
@@ -63,13 +79,14 @@ for epoch in range(5000):
         print(f'Epoch: {epoch:<10} Loss: {loss:<10.0f} LR: {lr}')
 '''
 
-opt.minimize(loss_fn,smallest_lr=1e-10,verbose=True, tol_const=1e-40)
+opt.minimize(loss_fn,smallest_lr=1e-10,verbose=True, tol_supp=1e-200)
 
 
 
 plt.scatter(x,y,zorder=2)
 print(1-measure.total_mass())
 measure.visualize()
+plt.show()
 
 '''
 plt.hist(measure.sample(10000),bins=50, density=True, range=[-4,4])
