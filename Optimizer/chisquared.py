2271ec4f49d9365f0af5bb88df006d4f762864a8
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-03-09 14:38:16 +0100

    Implement chi_squared (not working)

--- Optimizer/chisquared.py
+++ Optimizer/chisquared.py
@@ -6,3 +6,14 @@ l = torch.tensor([0., 0.2, 0.4, 0.6, 0.8, 1.])
 w = torch.tensor([0.3, 0.15, 0.05, 0.05, 0.15, 0.3])
 measure = pm.Measure(l, w)
 
+data = torch.randn(1000)
+
+def chi_squared(measure):
+    bins = torch.tensor([torch.argmin(abs(data[i] - measure[0].weights)) for i in range(len(data))])
+    bins_freq = []
+    print(measure[0.weights])
+    return sum(measure[0].weights**2/bins_freq)
+
+opt = pm.Optimizer(measure, lr=0.01)
+opt.minimize(chi_squared, verbose = True)
+measure.visualize()
