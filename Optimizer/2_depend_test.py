a2757005a0d2e24b004d84763ef2905ab0de6452
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-03-29 09:37:33 +0200

    Working visualisation of dependent measures

--- Optimizer/2_depend_test.py
+++ Optimizer/2_depend_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 N = 100
 x = torch.linspace(0, 5, N)
-y = (1 + torch.randn(N)) * x + 0.5 * torch.randn(N)
+y = (1 + torch.randn(N)) * x + 0.5 + torch.randn(N)
 
 plt.scatter(x, y)
 plt.show()
@@ -28,6 +28,24 @@ def loss_fn(measures):
 
 opt = pm.Optimizer(measure, lr = 1.0)
 opt.minimize(loss_fn, verbose = True)
-loc = torch.meshgrid(loc)
-plt.plot()
+
+maxi = opt.measures[0].locations[torch.argmax(opt.measures[0].weights)]
+print(maxi)
+plt.scatter(x,y)
+plt.plot([0,5],[0,5*maxi[0].item()+maxi[1]])
+plt.show()
+
+weights = []
+x = []
+y = []
+for i in range(M):
+    for j in range(M):
+        if opt.measures[0].weights[i*M+j].item() > 0.001:
+            weights.append(opt.measures[0].weights[i*M+j].item())
+            x.append(loc[i].item())
+            y.append(loc[j].item())
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.set_xlim(0,5)
+ax.set_ylim(0,5)
+ax.scatter(x,y,weights)
 plt.show()
