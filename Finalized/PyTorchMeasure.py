4dab1346983c5b126a2b4890775f0daa89a34bfb
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-03-30 14:26:28 +0200

    Update visualize with working code

--- Finalized/PyTorchMeasure.py
+++ Finalized/PyTorchMeasure.py
@@ -290,10 +290,15 @@ class Optimizer:
         return self.measures
 
     def visualize(self):
-        fig, axs = plt.subplots(2)
+        rows = torch.floor(torch.sqrt(torch.tensor(len(self.measures)))).item()
+        cols = torch.ceil(torch.sqrt(torch.tensor(len(self.measures)))).item()
+        fig, axs = plt.subplots(int(rows),int(cols))
+        if len(self.measures) == 1:
+            axs = [axs]
         fig.suptitle('Optimizer Visualization')
+        grads = [measure.weights.grad for measure in self.measures]
         for i, measure in enumerate(self.measures):
-            M, m = self.grads[i].max(), self.grads[i].min()
+            M, m = grads[i].max(), grads[i].min()
             wm = measure.weights.max()
             scaled_weights = measure.weights * (M - m) / wm * 0.25 + 1.3 * m
             support = measure.support()
@@ -302,7 +307,7 @@ class Optimizer:
                 axs[i].plot(measure.locations[support], torch.zeros(len(measure.weights))[support] + m,
                             '.', c='red', label=' Measure Support')
                 # Gradient
-                axs[i].plot(measure.locations, self.grads[i], c='green', label=' Gradient')
+                axs[i].plot(measure.locations, grads[i], c='green', label=' Gradient')
                 # Measure weights where there is support
                 axs[i].vlines(measure.locations[support], torch.zeros(len(measure.weights))[support] + 1.3 * m,
                               scaled_weights[support], colors='blue', label=' Measure Weights')
