2fb67d44ef0b6ef6e07da0120e1dacc7a9f7e09b
Hampus <hampeboy.ho@gmail.com>
2023-03-31 13:45:50 +0200

    parametric

--- Hampus/measureWithTorch.py
+++ Hampus/measureWithTorch.py
@@ -6,31 +6,60 @@ import matplotlib.pyplot as plt
 class NormalModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.m = torch.nn.Parameter(torch.randn(()))
-        self.s = torch.nn.Parameter(torch.randn(()))
 
-    def __str__(self):
-        return f'Mean: {self.m.item():.4f} and Std: {self.s.item():.4f}'
+        self.mean = None
+        self.std = None
+        k = 12
+
+        self.hidden_layers = torch.nn.Sequential(
+            torch.nn.Linear(1, k),
+            torch.nn.Sigmoid(),
+        )
+
+        self.mean_layer = torch.nn.Linear(k, 1)
+        self.std_layer = torch.nn.Linear(k, 1)
+        self.elu = torch.nn.ELU(1)
+
+    def forward(self, x):
+        hl = self.hidden_layers(x)
+        self.mean = self.mean_layer(hl)
+        s = self.std_layer(hl)
+        self.std = self.elu(s) + 1.
+
+        return self.mean, self.std
+
+    def __repr__(self):
+        return f'Mean: {self.mean.item():.5f} and Std: {self.std.item():.5f}'
 
 
+N = 200  # datapoints
 
-
-N = 1000 # datapoints
-
-x = torch.linspace(0, 10, N)
-y = torch.randn(N)
+x = torch.linspace(1, 5, N).view(-1, 1)
+y = (x)**2 -6*x + torch.tensor(np.random.normal(2., 0.2, N), dtype=torch.float).view(-1, 1)
 
 model = NormalModel()
-opt = torch.optim.Adam(model.parameters(), lr=0.001)
+opt = torch.optim.SGD(model.parameters(), lr=0.1)
+loss_fn = torch.nn.MSELoss()
 
 for epoch in range(1000):
-    loss = -(1 / np.sqrt(2 * np.pi) * torch.exp(-(y-model.m) / (model.s ** 2 / 2) / model.s)/model.s).log().sum()
-
-    if epoch % 10 == 0:
-        print(f'{epoch}:: Loss = {loss.item()}')
-
-    loss.backward()
     opt.zero_grad()
+    mean, std = model(x)
+    dist = torch.distributions.Normal(mean, std)
+    loss = -dist.log_prob(y).sum()
+    #kern = 1 / np.sqrt(2 * np.pi) * torch.exp(-(y- mean) ** 2 / 2)
+    #loss = loss_fn(mean, y)
+
+    if epoch % 50 == 0:
+        print(f'{epoch}:: Loss = {loss.item()}')
+    loss.backward()
     opt.step()
 
-print(model)
+x_plot = torch.linspace(1.05, 4.96, 40).view(-1, 1)
+mean, std = model(x_plot)
+mean = mean.detach().squeeze(-1)
+std = std.detach().squeeze(-1)
+plt.scatter(x, y, s=3)
+plt.errorbar(x=x_plot, y=mean, yerr=std)
+#plt.plot(x_plot, mean.detach())
+plt.show()
+
