de2e59d97247b7c44a5d064220a17ec36007a8a1
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-04-03 09:07:29 +0200

    Attempt to use KDENLLLoss on 3 var case

--- Optimizer/3_vars_test.py
+++ Optimizer/3_vars_test.py
@@ -1,6 +1,7 @@
 import torch
 import pytorch_measure as pm
 import matplotlib.pyplot as plt
+import numpy as np
 
 #torch.manual_seed(30)
 N = 1000
@@ -12,20 +13,39 @@ plt.show()
 
 M = 50 # <- number of locations on measure
 
+# Measures to optimize
 a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
 b = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
 c = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
 
+# Regression model
+def regression_model(param, x):
+     return param[0]*x**2+param[1]*x+param[0]
+
+# Distance from model prediction to true data
 def error(x, param, y): # a is location in measure (scalar), for example slope in linear regression
     return ((param[0] * x**2 + param[1] * x + param[2] - y).pow(2)).sum()
 
+# Indexing of the measures and computation of errors for each triplet
 measures = [a,b,c]
 idx = torch.tensor([[i, j, k] for i in range(len(measures[0].locations)) for j in range(len(measures[1].locations)) for k in range(len(measures[2].locations))])
 locs = torch.cat([measures[i].locations[idx[:, i]].unsqueeze(1) for i in range(len(measures))], 1)
 errors = torch.tensor([error(x, locs[i], y) for i in range(len(idx))])
+
+# MSE
 def loss_fn(measures: list[pm.Measure], n_samples=1000):
     probs = torch.cat([measures[i].weights[idx[:, i]].unsqueeze(1) for i in range(len(measures))], 1).prod(1)
     return errors.dot(probs)
+
+# Kernel
+def K(d):
+        return 1/np.sqrt(2*np.pi)*np.exp(-d**2/2)
+h=1.06*N**(-1/5)
+kde_mat = K((y.view(-1,1) - regression_model(locs, x)) / h)
+
+def KDElog_loss(measures):
+    probs = torch.cat([measures[i].weights[idx[:, i]].unsqueeze(1) for i in range(len(measures))], 1).prod(1)
+    return -(torch.matmul(kde_mat, probs) / (N*h)).log().sum()
 
 def log_prep(x, locs, y):
     loc_idx = []
@@ -37,7 +57,7 @@ def log_prep(x, locs, y):
 loc_index = log_prep(x,locs,y)
 def log_loss(measures: list[pm.Measure]):
     probs = torch.cat([measures[i].weights[idx[:, i]].unsqueeze(1) for i in range(len(measures))], 1).prod(1)
-    return -(probs[loc_index].log()).sum()
+    return -sum(torch.log(probs[loc_index]))
 
 alpha = 0.001
 bins = log_prep(x, locs, y)
@@ -48,7 +68,7 @@ def chi_squared(measures: list[pm.Measure]):
     return sum(probs**2/bins_freq)
 
 opt = pm.Optimizer([a, b, c], lr = 0.01)
-measures = opt.minimize(log_loss, max_epochs=1000, verbose = True, print_freq=1, smallest_lr=1e-20)
+measures = opt.minimize(KDElog_loss, max_epochs=1000, verbose = True, print_freq=1, smallest_lr=1e-20)
 a,b,c = measures[0],measures[1],measures[2]
 opt.visualize()
 plt.show()
@@ -61,4 +81,6 @@ plt.plot(x, aMax*x**2+bMax*x+cMax, 'r-')
 plt.plot(x, (aMax-aVar**0.5)*x**2+bMax*x+cMax, 'r--')
 plt.plot(x, (aMax+aVar**0.5)*x**2+bMax*x+cMax, 'r--')
 plt.show()
-print(aMax,aVar)
+print(a)
+print(b)
+print(c)
