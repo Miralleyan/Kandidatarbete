d2092e6289c3d9079727855b2c4a669fc4b20c13
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-04-24 10:52:36 +0200

    Add more modularity

--- Optimizer/results.py
+++ Optimizer/results.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import pytorch_measure as pm
+import scipy as sp
 
 # Functions for linear combination
 def h_1(x):
@@ -27,33 +28,19 @@ def log_lik(x, y, beta, h_all):
 # Confidence intervals
 def misses(x, y, mu, sigma):
     miss = 0
+    S = 1000
     for i in range(x.size(dim=0)):
-        sample = torch.normal(mean = float(mu[i]), std = float(sigma[i]), size = (1,1000)).squeeze(dim=0)
+        sample = torch.normal(mean = float(mu[i]), std = float(sigma[i]), size = (1,S)).squeeze(dim=0)
         sample = torch.sort(sample)[0]
-        if y[i] < sample[25] or y[i] > sample[974]:
+        if y[i] < sample[int(np.floor(S*0.05))] or y[i] > sample[int(np.ceil(S*0.95))-1]:
             miss += 1
+    c1 = sp.stats.binom.ppf(0.05,500,0.1)
+    c3 = sp.stats.binom.ppf(0.5,500,0.1)
+    c2 = sp.stats.binom.ppf(0.95,500,0.1)
+    print(c1,c3,c2)
     return miss
 
-# Linear regression two variables
-h = [h_1, h_2]
-
-torch.seed = (2)
-N = 500
-x = torch.linspace(-4, 4, N)
-y = torch.squeeze((-3+torch.randn(N)) * x + (torch.normal(mean=1.0,std=1,size=(1,N))), 0)
-y.requires_grad = False
-
-#- Theoretical solution -
-h_1_data = h_1(x)
-h_2_data = h_2(x)
-h_all = torch.transpose(torch.stack([h_1_data, h_2_data], 0), 0, 1)
-
-plt.scatter(x, y)
-plt.show()
-
-mu = torch.tensor([0., 0.], dtype=float, requires_grad=True)
-sigma = torch.tensor([1., 1.], dtype=float, requires_grad=True)
-def runTheoretical(epochs):
+def runTheoretical(x, y, h_all, mu, sigma, epochs):
     beta = [mu, sigma]
     optimizer = torch.optim.Adam(beta,lr=0.1, maximize=True)
     for epoch in range(epochs):
@@ -63,105 +50,147 @@ def runTheoretical(epochs):
         optimizer.step()
         if epoch%10==0:
             print(epoch, mu, sigma)
-    # mu = beta[0].detach().numpy()
-    # sigma = beta[1].detach().numpy()
-    return [m(mu, h_all[i,:]).detach().numpy() for i in range(N)], [(sigma_2(sigma, h_all[i,:])**0.5).detach().numpy() for i in range(N)]
+    return [m(mu, h_all[i,:]).detach().numpy() for i in range(x.size(dim=0))], [(sigma_2(sigma, h_all[i,:])**0.5).detach().numpy() for i in range(x.size(dim=0))]
 
-plt.scatter(x,y,alpha=0.5)
-mu, sigma = runTheoretical(400)
-sigma2 = 2*sigma
-plt.plot(x, mu, 'r-')
-plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
-plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
-plt.fill_between(x, [mu[i]+sigma[i] for i in range(N)], [mu[i]-sigma[i] for i in range(N)], alpha = 0.2)
-ax = plt.gca()
-ax.set_ylim([-10, 30])
-
-#- Our method -
+# Linear regression two variables
+# Linear model
 def linModel(x,params):
     return params[1]*x+params[0]
 
-M = 50
-a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
-b = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
-opt = pm.Optimizer([b,a], 'KDEnll', lr = 0.1)
-[b,a] = opt.minimize([x,y], linModel, max_epochs=400)
-aMean = torch.sum(a.locations*a.weights).detach()
-bMean = torch.sum(b.locations*b.weights).detach()
-plt.plot(x, aMean*x+bMean, 'b-')
-print(misses(x, y, mu, sigma))
+def linTest():
+    M = 50
+    a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    b = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    opt = pm.Optimizer([b,a], 'KDEnll', lr = 0.1)
+    [b,a] = opt.minimize([x,y], linModel, max_epochs=400)
+    aMean = torch.sum(a.locations*a.weights).detach()
+    bMean = torch.sum(b.locations*b.weights).detach()
+    plt.plot(x, aMean*x+bMean, 'b-')
+    print(misses(x, y, mu, sigma))
 
-plt.show()
+    plt.show()
+
+    h = [h_1, h_2]
+
+    torch.seed = (2)
+    N = 500
+    x = torch.linspace(-4, 4, N)
+    y = torch.squeeze((-3+torch.randn(N)) * x + (torch.normal(mean=1.0,std=1,size=(1,N))), 0)
+    y.requires_grad = False
+
+    #- Theoretical solution -
+    h_1_data = h_1(x)
+    h_2_data = h_2(x)
+    h_all = torch.transpose(torch.stack([h_1_data, h_2_data], 0), 0, 1)
+
+    plt.scatter(x, y)
+    plt.show()
+
+    mu = torch.tensor([0., 0.], dtype=float, requires_grad=True)
+    sigma = torch.tensor([1., 1.], dtype=float, requires_grad=True)
+
+    plt.scatter(x,y,alpha=0.5)
+    mu, sigma = runTheoretical(x,y,h_all,mu, sigma, 400)
+    sigma2 = 2*sigma
+    plt.plot(x, mu, 'r-')
+    plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
+    plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
+    plt.fill_between(x, [mu[i]+sigma[i] for i in range(N)], [mu[i]-sigma[i] for i in range(N)], alpha = 0.2)
+    ax = plt.gca()
+    ax.set_ylim([-10, 30])
 
 #-- Quadratic regression --
-torch.seed = (1)
-N = 500
-x = torch.linspace(-4, 4, N)
-y = torch.squeeze((torch.normal(mean=1.0,std=1,size=(1,N))) * x**2 + (-3+torch.randn(N)) * x + (torch.normal(mean=1.0,std=1,size=(1,N))), 0)
-y.requires_grad = False
-
-#- Theoretical solution -
-h = [h_1, h_2, h_3]
-
-h_1_data = h_1(x)
-h_2_data = h_2(x)
-h_3_data = h_3(x)
-h_all = torch.transpose(torch.stack([h_1_data, h_2_data, h_3_data], 0), 0, 1)
-
-plt.scatter(x, y)
-plt.show()
 
 #- Our method -
-def model(x,params):
+def quadModel(x,params):
     return params[2]*x**2+params[1]*x+params[0]
 
-M = 50
-a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
-b = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
-c = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
-opt = pm.Optimizer([c,b,a], 'KDEnll', lr = 0.1)
-[c,b,a] = opt.minimize([x,y], model, max_epochs=400)
-aMean = torch.sum(a.locations*a.weights).detach()
-bMean = torch.sum(b.locations*b.weights).detach()
-cMean = torch.sum(c.locations*c.weights).detach()
+def quadTest():
+    torch.seed = (1)
+    N = 500
+    x = torch.linspace(-4, 4, N)
+    y = torch.squeeze((torch.normal(mean=1.0,std=1,size=(1,N))) * x**2 + (-3+torch.randn(N)) * x + (torch.normal(mean=1.0,std=1,size=(1,N))), 0)
+    y.requires_grad = False
 
-plt.scatter(x,y, alpha=0.5)
-plt.plot(x, aMean*x**2+bMean*x+cMean, 'b-')
-mu = torch.tensor([0., 0.], dtype=float, requires_grad=True)
-sigma = torch.tensor([1., 1.], dtype=float, requires_grad=True)
-mu, sigma = runTheoretical(400)
-sigma2 = 2*sigma
-plt.plot(x, mu, 'r-')
-plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
-plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
-plt.fill_between(x, [mu[i]+sigma[i] for i in range(N)], [mu[i]-sigma[i] for i in range(N)], alpha = 0.2)
-ax = plt.gca()
-ax.set_ylim([-10, 30])
-plt.show()
-print(misses(x,y,mu,sigma))
+    #- Theoretical solution -
+    h = [h_1, h_2, h_3]
 
+    h_1_data = h_1(x)
+    h_2_data = h_2(x)
+    h_3_data = h_3(x)
+    h_all = torch.transpose(torch.stack([h_1_data, h_2_data, h_3_data], 0), 0, 1)
 
+    plt.scatter(x, y)
+    plt.show()
+
+    M = 50
+    a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    b = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    c = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    opt = pm.Optimizer([c,b,a], 'KDEnll', lr = 0.1)
+    [c,b,a] = opt.minimize([x,y], quadModel, max_epochs=400)
+    aMean = torch.sum(a.locations*a.weights).detach()
+    bMean = torch.sum(b.locations*b.weights).detach()
+    cMean = torch.sum(c.locations*c.weights).detach()
+
+    plt.scatter(x,y, alpha=0.5)
+    plt.plot(x, aMean*x**2+bMean*x+cMean, 'b-')
+    mu = torch.tensor([0., 0., 0.], dtype=float, requires_grad=True)
+    sigma = torch.tensor([1., 1., 1.], dtype=float, requires_grad=True)
+    mu, sigma = runTheoretical(x, y, h_all, mu, sigma, 400)
+    sigma2 = 2*sigma
+    plt.plot(x, mu, 'r-')
+    plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
+    plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
+    plt.fill_between(x, [mu[i]+sigma[i] for i in range(N)], [mu[i]-sigma[i] for i in range(N)], alpha = 0.2)
+    ax = plt.gca()
+    ax.set_ylim([-10, 30])
+    plt.show()
+    print(misses(x,y,mu,sigma))
+
+def constModel(x,params):
+    return params[0]*1
 #- Constant -
-torch.seed = (1)
-N = 500
-x = torch.linspace(-4, 4, N)
-y = torch.squeeze((torch.normal(mean=1.0,std=1,size=(1,N))) * x**2 + (-3+torch.randn(N)) * x + (torch.normal(mean=1.0,std=1,size=(1,N))), 0)
-y.requires_grad = False
+def normTest():
+    torch.seed = (1)
+    N = 500
+    x = torch.linspace(-4, 4, N)
+    y = torch.squeeze((torch.normal(mean=1.0,std=1,size=(1,N))), 0)
+    y.requires_grad = False
 
-#- Theoretical solution -
-h = h_1
+    #- Theoretical solution -
+    h = h_1
 
-h_1_data = h_1(x)
-h_all = torch.transpose(torch.stack([h_1_data, h_2_data, h_3_data], 0), 0, 1)
+    h_1_data = h_1(x)
+    h_all = torch.transpose(torch.stack([h_1_data], 0), 0, 1)
 
-plt.scatter(x, y)
-plt.show()
+    plt.scatter(x, y)
+    plt.show()
 
-mu = torch.tensor([0.], dtype=float, requires_grad=True)
-sigma = torch.tensor([1.], dtype=float, requires_grad=True)
-mu, sigma = runTheoretical(400)
-sigma2 = 2*sigma
-plt.plot(x, mu, 'r-')
-plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
-plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
-plt.show()
+    mu = torch.tensor([0.], dtype=float, requires_grad=True)
+    sigma = torch.tensor([1.], dtype=float, requires_grad=True)
+    mu, sigma = runTheoretical(x, y, h_all, mu, sigma, 400)
+    sigma2 = 2*sigma
+    print(misses(x,y,mu,sigma))
+
+    # plt.scatter(x,y, alpha=0.5)
+    # plt.plot(x, mu, 'r-')
+    # plt.plot(x, [mu[i]+sigma2[i] for i in range(N)], 'r--') # Upper bound confidence interval
+    # plt.plot(x, [mu[i]-sigma2[i] for i in range(N)], 'r--') # Lower bound confidence interval
+    # plt.show()
+
+    #- Our method -
+    M = 50
+    a = pm.Measure(torch.linspace(-10, 10, M), torch.ones(M) / M)
+    opt = pm.Optimizer([a], 'KDEnll', lr = 0.1)
+    [a] = opt.minimize([x,y], constModel, max_epochs=400)
+    checker = pm.Check(opt, constModel, x, y, 0.05)
+    print(checker.check())
+
+    counts, bins = np.histogram(y,bins=20)
+    plt.stairs(counts/sum(counts), bins)
+    plt.plot(x, sp.stats.norm.pdf(x, mu, sigma))
+    plt.plot(a.locations.detach().numpy(), a.weights.detach().numpy())
+    plt.show()
+
+normTest()
