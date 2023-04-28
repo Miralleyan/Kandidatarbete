d1d579ea842f058c6f6575fd39fd8e01a9dbd33a
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-04-28 11:39:31 +0200

    Update so that it works again

--- Optimizer/three_measure_quad_results.py
+++ Optimizer/three_measure_quad_results.py
@@ -10,21 +10,84 @@ import time
 import linear_combination_optimizer as lco
 
 #torch.manual_seed(30) # <-- if seed is wanted
-N = 500
+N = 400
 x = torch.linspace(-3, 5, N)
-print(x.size())
 
 
 # Plot the data points
 #plt.scatter(x, y)
-# plt.show()
+plt.show()
+
+# Number of locations of measure
+M = 20
+
+# Linear regression model
+def regression_model(x,list):
+     return list[0]*x**2+list[1]*x+list[2]
+
+runs = 2
+maxepochs = 400
+alpha = torch.randn(runs)
+beta = torch.randn(runs)
+gamma = torch.randn(runs)
+
+success=[]
+for i in range(runs):
+
+    # Measure for slope (a) and intercept (b) of linear model
+    a = pm.Measure(torch.linspace(-4, 4, M), torch.ones(M) / M)
+    b = pm.Measure(torch.linspace(-2, 6, M), torch.ones(M) / M)
+    c = pm.Measure(torch.linspace(-2, 6, M), torch.ones(M) / M)
+
+    measures = [a,b,c]
+    y = (torch.randn(N)+alpha[i]) * x**2 + (beta[i]+torch.randn(N)*x + gamma[i]+torch.randn(N))
+    # Instance of optimizer
+    opt = pm.Optimizer(measures, "KDEnll", lr = 0.1)
+    # Call to minimizer
+    new_mes=opt.minimize([x,y],regression_model,max_epochs=maxepochs,verbose = True, print_freq=100, smallest_lr=1e-10)
+    # Visualize measures and gradient
+    new_mes[0].visualize()
+    #plt.show()
+    new_mes[1].visualize()
+    #plt.show()
+
+    check=pm.Check(opt,regression_model,x,y,normal=True,Return=True)
+    l,u,miss=check.check()
+    #check.check()
+    success.append(l<=miss and miss<=u)
+
+print(f'{sum(success)} successes')
+print(f'Our method succeeds {100*sum(success)/runs}% of the time')
+
+
+# Linear combination method
+# Functions for linear combination
+def h_1(x):
+    return x*0+1
+def h_2(x):
+    return x
+def h_3(x):
+    return x**2
+
+def m(mu, h_x):
+    return (mu * h_x).sum()
+
+def sigma_2(sigma, h_x):
+    return (sigma * h_x).pow(2).sum()
+
+def log_normal_pdf(y, mu, sigma_2):
+    return -1/2*(torch.log(2*np.pi*(sigma_2)) + ((y-mu)**2/sigma_2))
+
+def log_lik(y, beta, h_all):
+    return sum([log_normal_pdf(y_i, m(beta[0], h_all[i]), sigma_2(beta[1], h_all[i])) for i, y_i in enumerate(y)])
+
 
 # Confidence intervals
 def misses(x, y, mu, sigma):
     miss = 0
     S = 1000
     for i in range(x.size(dim=0)):
-        sample = torch.normal(mean = float(mu[i]), std = float(sigma[i]), size = (S,1)).squeeze(dim=0)
+        sample = torch.normal(mean = float(mu[i]), std = float(sigma[i]), size = (1,S)).squeeze(dim=0)
         sample = torch.sort(sample)[0]
         if y[i] < sample[int(np.floor(S*0.025))] or y[i] > sample[int(np.ceil(S*0.975))-1]:
             miss += 1
@@ -34,6 +97,34 @@ def misses(x, y, mu, sigma):
     print(c1,c3,c2)
     print(f"CI: [{c1}, {c2}], Misses: {miss}, Within CI: {c1<=miss<=c2}")
     return c1, c2, miss
+
+# Linear combs
+def runTheoretical(x, y, h_all, mu, sigma, epochs):
+    beta = [mu, sigma]
+    optimizer = torch.optim.Adam(beta,lr=0.1, maximize=True)
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        loss = log_lik(y, beta, h_all)
+        loss.backward()
+        optimizer.step()
+        if epoch%10==0:
+            print(epoch, mu, sigma)
+    return [m(mu, h_all[i,:]).detach().numpy() for i in range(x.size(dim=0))], [(sigma_2(sigma, h_all[i,:])**0.5).detach().numpy() for i in range(x.size(dim=0))]
+
+success=[]
+for i in range(runs):
+    y = (torch.randn(N)+alpha[i]) * x**2 + (beta[i]+torch.randn(N)*x + gamma[i]+torch.randn(N))
+    h = [h_1, h_2, h_3]
+
+    #- Theoretical solution -
+    opt = lco.Optimizer(x,y,order=3)
+    mu, sigma, conv_epoch, conv_time = opt.optimize(epochs = 500, test = True)
+    l, u, miss = misses(x,y,mu,sigma)
+    success.append(l<=miss and miss<=u)
+
+print(f'{sum(success)} successes')
+print(f'Linear combination method succeeds {100*sum(success)/runs}% of the time')
+
 
 # Polynomial nn method
 def eval_powers_of_x(x, n):
@@ -74,78 +165,10 @@ def check_within_two_stddeviations(x, y):
     hits = (a <= 2).sum()
     return hits / len(x)
 
-# Functions for linear combination
-def h_1(x):
-    return x*0+1
-def h_2(x):
-    return x
-def h_3(x):
-    return x**2
-
-def m(mu, h_x):
-    return (mu * h_x).sum()
-
-def sigma_2(sigma, h_x):
-    return (sigma * h_x).pow(2).sum()
-
-def log_normal_pdf(y, mu, sigma_2):
-    return -1/2*(torch.log(2*np.pi*(sigma_2)) + ((y-mu)**2/sigma_2))
-
-def log_lik(y, beta, h_all):
-    return sum([log_normal_pdf(y_i, m(beta[0], h_all[i]), sigma_2(beta[1], h_all[i])) for i, y_i in enumerate(y)])
-
-# Linear combs
-def runTheoretical(x, y, h_all, mu, sigma, epochs):
-    beta = [mu, sigma]
-    optimizer = torch.optim.Adam(beta,lr=0.1, maximize=True)
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        loss = log_lik(y, beta, h_all)
-        loss.backward()
-        optimizer.step()
-        if epoch%10==0:
-            print(epoch, mu, sigma)
-    return [m(mu, h_all[i,:]).detach().numpy() for i in range(x.size(dim=0))], [(sigma_2(sigma, h_all[i,:])**0.5).detach().numpy() for i in range(x.size(dim=0))]
-
-# Number of locations of measure
-M = 20
-
-# Linear regression model
-def regression_model(x,list):
-     return list[0]*x**2+list[1]*x+list[2]
-
-runs = 1
-maxepochs = 1000
-alpha = torch.randn(runs)
-beta = torch.randn(runs)
-gamma = torch.randn(runs)
-
-success_pm=[]
-success_nn = []
-success_lc = []
+success=[]
+M = []
+S = []
 for i in range(runs):
-    # Measure geadient descent
-    # Measure for slope (a) and intercept (b) of linear model
-    a = pm.Measure(torch.linspace(-4, 4, M), torch.ones(M) / M)
-    b = pm.Measure(torch.linspace(-2, 6, M), torch.ones(M) / M)
-    c = pm.Measure(torch.linspace(-2, 6, M), torch.ones(M) / M)
-
-    measures = [a,b,c]
-    y = (torch.randn(N)+alpha[i]) * x**2 + (beta[i]+torch.randn(N)*x + gamma[i]+torch.randn(N))
-    # Instance of optimizer
-    opt = pm.Optimizer(measures, "KDEnll", lr = 0.1)
-    # Call to minimizer
-    [a,b,c]=opt.minimize([x,y],regression_model,max_epochs=maxepochs,verbose = True, print_freq=100, smallest_lr=1e-10)
-    a_mean = sum(a.weights*a.locations).detach()
-    b_mean = sum(b.weights*b.locations).detach()
-    c_mean = sum(c.weights*c.locations).detach()
-
-    check=pm.Check(opt,regression_model,x,y,normal=True,Return=True)
-    l,u,miss=check.check()
-    #check.check()
-    success_pm.append(l<=miss and miss<=u)
-
-
     # Polynomial nn method
     x_unsq = x.view(-1, 1)
     y = (torch.normal(1,0.2,(1, N))+alpha[i]) * x**2 + (beta[i]+torch.randn(N)*x + gamma[i]+torch.randn(N))
@@ -176,35 +199,13 @@ for i in range(runs):
 
     m, var = model(x_unsq)
     s = var.sqrt()
+    M.append(m)
+    S.append(s)
 
     l,u,miss=misses(x,y,m,s)
-    #check.check()
-    success_nn.append(l<=miss and miss<=u)
+    success.append(l<=miss and miss<=u)
 
-    # plt.scatter(x_unsq, y, marker='.')
-    # plt.plot(x_unsq, m.detach(), 'r')
-    # plt.fill_between(x_unsq.squeeze(), (m.detach()-s.detach()).squeeze(), (m.detach()+s.detach()).squeeze(), alpha=0.5)
-    # plt.show()
+print(f'{sum(success)} successes')
+print(f'Polynomial nn method succeeds {100*sum(success)/runs}% of the time')
 
-
-    # Linear combination method
-    y = (torch.randn(N)+alpha[i]) * x**2 + (beta[i]+torch.randn(N)*x + gamma[i]+torch.randn(N))
-    h = [h_1, h_2, h_3]
-
-    #- Theoretical solution -
-    opt = lco.Optimizer(x,y,order=3)
-    t1 = time.time()
-    mu, sigma = opt.optimize(epochs=1000)
-    t2 = time.time()
-    print(f'Time: {t2-t1}')
-    l, u, miss = misses(x,y,mu,sigma)
-    success_lc.append(l<=miss and miss<=u)
-
-    # Plotting
-    print(alpha[i], beta[i], gamma[i])
-    plt.scatter(x,y)
-    plt.plot(x, a_mean*x**2+b_mean*x+c_mean, 'b')
-    plt.plot(x_unsq, m.detach(), 'r')
-    plt.plot(x, [x[i]*mu[i] for i in range(x.size(dim=0))], 'g')
-    plt.show()
     
