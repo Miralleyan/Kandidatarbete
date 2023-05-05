8fa348b4cd3195f38b54053cb405c3a7750f16fa
Miralleyan <odesjo@chalmers.se>
2023-05-05 11:30:11 +0200

    Cleaned the checks

--- Optimizer/Check3.py
+++ Optimizer/Check3.py
@@ -1,32 +1,20 @@
+import matplotlib.pyplot as plt
 import torch
 import pytorch_measure as pm
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-import json
+import json 
 import math
-'''
-#torch.manual_seed(30) # <-- if seed is wanted
-N = 100
-x = torch.linspace(-3, 5, N)
- 
 
-# Plot the data points
-#plt.scatter(x, y)
-
-# Number of locations of measure
-M = 17
-'''
 
 
 # Linear regression model
 def regression_model(x,list):
      return list[0]*x**2+list[1]*x+list[2]
 
-
-
-
+#Load data of parameters
 param=np.load('../Finalized/test_data/params.npy')
+
 for length in [100,500,1000]:
     success=[]
     tid=[]
@@ -41,7 +29,7 @@ for length in [100,500,1000]:
         #plt.show()
         M=length #Amount of datapoints
 
-        s=2
+        s=2 #Decides width of the measures
         aU=math.ceil(param[0][3*i+2]+s*param[1][3*i+2])
         aL=math.floor(param[0][3*i+2]-s*param[1][3*i+2])
         bU=math.ceil(param[0][3*i+1]+s*param[1][3*i+1])
@@ -57,16 +45,14 @@ for length in [100,500,1000]:
         a = pm.Measure(torch.linspace(aU, aL, Na), torch.ones(Na).double() / Na)
         b = pm.Measure(torch.linspace(bU, bL, Nb), torch.ones(Nb).double() / Nb)
         c= pm.Measure(torch.linspace(cU, cL, Nc), torch.ones(Nc).double() / Nc)
-
-
         measure = [a,b,c]
 
-        #y = (torch.randn(N)+4)*x**2+(torch.randn(N)+-0.5) * x + (2+torch.randn(N))
         # Instance of optimizer
         opt = pm.Optimizer(measure, "KDEnll", lr = 0.1)
-        # Call to minimizer
 
+        # Call to minimizer
         new_mes,time,iteration=opt.minimize([x,y],regression_model,max_epochs=4000,verbose = False, print_freq=100, smallest_lr=1e-10,test=True)
+
         # Visualize measures and gradient
         new_mes[0].visualize()
         #plt.show()
@@ -75,8 +61,10 @@ for length in [100,500,1000]:
         new_mes[2].visualize() 
         #plt.show()
 
+        #Run tests and save the results
         check=pm.Check(opt,regression_model,x,y,normal=False,Return=True)
         l,u,miss=check.check()
+
         success.append(l<=miss and miss<=u)
         tid.append(time)
         epoch.append(iteration)
@@ -84,10 +72,7 @@ for length in [100,500,1000]:
             measures.append([new_mes[i].locations.tolist(),new_mes[i].weights.tolist()])
 
     data=[measures,sum(tid)/len(tid),sum(epoch)/len(epoch),sum(success)/len(success)]
-    with open(f"resultat_samuel/Sergey3M_{M}.json", "w") as outfile:
+    with open(f"resultat/Sergey3_{M}.json", "w") as outfile:
         outfile.write(json.dumps(data))
 
 
-print(sum(success)/len(success))
-print(sum(tid)/len(tid))
-print(sum(epoch)/len(epoch))
