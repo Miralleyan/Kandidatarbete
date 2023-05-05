2201189efb753c3d58c974baaf2c86b00e8d13be
Joel Wall <joelwal@chalmers.se>
2023-05-05 10:48:19 +0200

    improved way variance is stored, intervals

--- Finalized/boston/gaussian.py
+++ Finalized/boston/gaussian.py
@@ -17,6 +17,9 @@ y = data[:, -1]
 x = torch.tensor(x, dtype=torch.float32)
 y = torch.tensor(y, dtype=torch.float32).view(-1, 1)
 
+x -= x.mean(0)
+x /= x.std(0)
+
 # # normalize data
 # x -= x.mean(axis=0)
 # x[:, :-1] /= x[:, :-1].std(axis=0)
@@ -25,39 +28,25 @@ class GaussianRegression(nn.Module):
     def __init__(self, input_features):
         super().__init__()
         self.pred_layer = nn.Linear(input_features, 1)
-        self.dist_vars_log = nn.Parameter(torch.zeros(input_features, requires_grad=True)) # want to force variance to be greater than zero.
+        self.dist_std = nn.Parameter(torch.ones(input_features, requires_grad=True)) # want to force variance to be greater than zero.
         self.std_default = nn.Parameter(torch.tensor(1., requires_grad=True))
         #self.var_layer = nn.Linear(input_features, 1)
     
     def forward(self, x):
         y_pred = self.pred_layer(x)
-        var_pred = (x.pow(2) * self.dist_vars_log.exp()).sum(1) + self.std_default.pow(2)
+        var_pred = (x.pow(2) * self.dist_std.pow(2)).sum(1) + self.std_default.pow(2)
         return y_pred, var_pred
 
 
 model = GaussianRegression(13)
-opt = optim.SGD(model.parameters(), lr=0.02)
-criterion = nn.GaussianNLLLoss()
-
-prep_opt = optim.Adam(model.pred_layer.parameters(), lr=0.1)
-prep_criterion = nn.MSELoss()
+opt = optim.SGD(model.parameters(), lr=0.01)
+criterion = nn.GaussianNLLLoss(full=True)
 
 dataset = TensorDataset(x, y)
-loader = DataLoader(dataset, batch_size=256, shuffle=True)
-
-for epoch in range(5000):
-    prep_opt.zero_grad()
-    y_pred, _ = model(x)
-    loss = prep_criterion(y_pred, y)
-    loss.backward()
-    prep_opt.step()
-
-print(list(model.pred_layer.parameters()))
-plt.scatter(y.detach().numpy(), y_pred.detach().numpy())
-plt.show()
+loader = DataLoader(dataset, batch_size=200, shuffle=True)
 
 
-for epoch in range(20000):
+for epoch in range(15000):
     for x_sample, y_sample in loader:
         opt.zero_grad()
 
@@ -70,12 +59,20 @@ for epoch in range(20000):
     if epoch % 100 == 99 or epoch < 10:
         print(f"{epoch + 1}: {loss.item()}")
 
-#print(f"mean: {mean.detach().numpy()}")
-#print(f"std: {(1 / var_inv).sqrt().detach().numpy()}")
+y_pred, var_pred = model(x)
+print(f"mean prediction parameters: {list(model.pred_layer.parameters())}")
+print(f"variance prediction: {1 * model.dist_std.pow(2)}")
+print(f"variance offset: {model.std_default ** 2}")
 
-y_pred, _ = model(x)
-print(list(model.parameters()))
+y_np = y.squeeze().detach().numpy()
+y_pred_np = y_pred.squeeze().detach().numpy()
 
-plt.scatter(y.detach().numpy(), y_pred.detach().numpy())
+plt.scatter(y_np, y_pred_np)
 plt.plot([0, 50], [0, 50])
+plt.ylabel('predicted price')
+plt.xlabel('actual price')
 plt.show()
+
+hits = ((y - y_pred).abs().squeeze() < 1.96 * var_pred.sqrt()).sum()
+proportion = hits / 506
+print(f"proportion of points within 95% interval: {proportion}")
