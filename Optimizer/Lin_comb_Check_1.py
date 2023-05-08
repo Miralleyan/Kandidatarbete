d8a6626320056069804c7e386c6b0510644ac4da
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2023-05-08 17:12:03 +0200

    Small changes

--- Optimizer/Lin_comb_Check_1.py
+++ Optimizer/Lin_comb_Check_1.py
@@ -1,38 +1,34 @@
-# File for comparing results of three measure quadratic
-# regression between methods
-
 import torch
-import pytorch_measure as pm
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy as sp
 import linear_combination_optimizer as lco
-from tqdm import tqdm
 import json
 
-# Confidence intervals
+# Calculate confidence intervals and returns amount of misses
 def misses(x, y, mu, sigma):
     miss = 0
     S = 1000
     for i in range(x.size(dim=0)):
+        # Generate sample from model
         sample = torch.normal(mean = float(mu[i]), std = float(sigma[i]), size = (1,S)).squeeze(dim=0)
         sample = torch.sort(sample)[0]
         if y[i] < sample[int(np.floor(S*0.025))] or y[i] > sample[int(np.ceil(S*0.975))-1]:
             miss += 1
+    # Calculate confidence interval
     c1 = sp.stats.binom.ppf(0.025,y.size(dim=0),0.05)
-    c3 = sp.stats.binom.ppf(0.5,y.size(dim=0),0.05)
     c2 = sp.stats.binom.ppf(0.975,y.size(dim=0),0.05)
-    print(c1,c3,c2)
     print(f"CI: [{c1}, {c2}], Misses: {miss}, Within CI: {c1<=miss<=c2}")
     return c1, c2, miss
 
-success=[]
+# For each data size
 for length in [100, 500, 1000]:
+    # Lists for storing results
     success=[]
     time=[]
     epoch=[]
     means=[]
     std=[]
+    # Run regressions 50 times
     for i in range(50):
         x = torch.linspace(-5, 5, length)
         y = np.load(f'../Finalized/test_data/data_{length}_y_{i}.npy')
@@ -46,6 +42,7 @@ for length in [100, 500, 1000]:
         means.append(mu)
         std.append(sigma)
     
+    # Save results in a json file
     results = [means, std, sum(time)/len(time), sum(epoch)/(len(epoch)), float(100*sum(success)/len(success))]
     with open(f"Lin_comb_results/lin_comb_results_{length}_y.json", "w") as outfile:
         outfile.write(json.dumps(results))
--- Optimizer/Lin_comb_Check_2.py
+++ Optimizer/Lin_comb_Check_2.py
@@ -1,16 +1,10 @@
-# File for comparing results of three measure quadratic
-# regression between methods
-
 import torch
-import pytorch_measure as pm
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy as sp
 import linear_combination_optimizer as lco
-from tqdm import tqdm
 import json
 
-# Confidence intervals
+# Calculate confidence intervals and returns amount of misses
 def misses(x, y, mu, sigma):
     miss = 0
     S = 1000
@@ -19,20 +13,21 @@ def misses(x, y, mu, sigma):
         sample = torch.sort(sample)[0]
         if y[i] < sample[int(np.floor(S*0.025))] or y[i] > sample[int(np.ceil(S*0.975))-1]:
             miss += 1
+    # Calculate confidence interval
     c1 = sp.stats.binom.ppf(0.025,y.size(dim=0),0.05)
-    c3 = sp.stats.binom.ppf(0.5,y.size(dim=0),0.05)
     c2 = sp.stats.binom.ppf(0.975,y.size(dim=0),0.05)
-    print(c1,c3,c2)
     print(f"CI: [{c1}, {c2}], Misses: {miss}, Within CI: {c1<=miss<=c2}")
     return c1, c2, miss
 
-success=[]
+# For each data size
 for length in [100, 500, 1000]:
+    # Lists for storing results
     success=[]
     time=[]
     epoch=[]
     means=[]
     std=[]
+    # Run regressions 50 times
     for i in range(50):
         x = torch.linspace(-5, 5, length)
         y = np.load(f'../Finalized/test_data/data_{length}_y_lin_{i}.npy')
@@ -41,12 +36,11 @@ for length in [100, 500, 1000]:
 
         l, u, miss = misses(x,torch.tensor(y),mu,sigma)
         success.append(l<=miss and miss<=u)
-        if conv_time != None:
-            time.append(conv_time)
         epoch.append(conv_epoch)
         means.append(mu)
         std.append(sigma)
     
+    # Save results in a json file
     results = [means, std, sum(time)/len(time), sum(epoch)/(len(epoch)), float(100*sum(success)/len(success))]
     with open(f"Lin_comb_results/lin_comb_results_{length}_y_lin.json", "w") as outfile:
         outfile.write(json.dumps(results))
